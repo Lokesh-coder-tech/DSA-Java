5e54364d09d70fef022460bde8d4178f6d94c690
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-01 09:07:38 +0530

    DSA Java setup with first program

---
+++ src/Day02.java
@@ -0,0 +1,24 @@
+//Variables and Data types
+//variables - variable is a container used to store data that can change during program execution
+//Data Types (Primitive Data)  - These are the Basic built-in data types
+public class Day02 {
+    public static void main(String[] args) {
+        byte a = 10;      //1 bytes
+        short b = 200;    //2 bytes
+        int c = 1000;     //4 bytes
+        long d = 10000L;  //8 bytes
+        float e = 5.5f;   //4 bytes
+        double f = 5.555; //8 bytes
+        char g = 'A';     //2 bytes
+        boolean h = true; //1 bit
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
