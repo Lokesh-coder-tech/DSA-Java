edfcfa94a5c1a6ce0420d6dad1984d5900fbb285
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-02 19:24:11 +0530

    Understand different operators in Java

---
+++ src/Day03.java
@@ -0,0 +1,75 @@
+//operators
+public class Day03 {
+    public static void main(String[] args) {
+        //Arithmetic operator---->
+//        int a = 10;
+//        int b = 5;
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a/b);
+//        System.out.println(a % b);
+
+        //Relational operator---->
+//        int a = 20;
+//        int b = 30;
+//
+//        System.out.println(a == b);
+//        System.out.println(a != b);
+//        System.out.println(a > b);
+//        System.out.println(a < b);
+//        System.out.println(a >= b);
+//        System.out.println(a <= b);
+
+        //Logical operator---->
+//        int a = 15;
+//        int b = 25;
+//        int c = 35;
+//
+//        System.out.println(a<b && b>c);
+//        System.out.println(a<b || b>c);
+
+        //Assignment operator----->
+//        int a = 12;
+//        int b = a;
+//
+//        System.out.println(b);
+//        System.out.println(a += 5);
+//        System.out.println(a -= 3);
+//        System.out.println(a *= 2);
+//        System.out.println(a /= 2);
+//        System.out.println(a %= 3);
+
+        //Unary operator- Icrement/Decrement Operator
+//        int a = 13;
+//        System.out.println(a);
+//        int preInc = ++a;
+//        System.out.println(preInc);
+//        int postInc = a++;
+//        System.out.println(postInc);
+//        System.out.println(a);
+//
+//        int b = 16;
+//        System.out.println(b);
+//        int preDec = --b;
+//        System.out.println(preDec);
+//        int postDec = b--;
+//        System.out.println(postDec);
+//        System.out.println(b);
+
+        //Ternary operator ---->
+//        int a = 40;
+//        int b = 50;
+//
+//        System.out.println(a > b ? "Jai Hind Dosto!!" : "Namaskaram");
+//        System.out.println(a < b ? "Jai Hind Dosto!!" : "Namaskaram");
+
+        //instanceof operator --->
+//        String name = "Lokesh";
+//        String surName = "Sharma";
+//        System.out.println(name instanceof String);
+//        System.out.println(surName instanceof String);
+
+    }
+}
