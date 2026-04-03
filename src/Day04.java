4364614c4d6c04aec7da4a3d33d8ae752947f165
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-03 18:31:46 +0530

    Understand How to take inputs in Java

---
+++ src/Day04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day04 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int number = sc.nextInt();
+        System.out.println("Enter second number");
+        int number2 = sc.nextInt();
+        System.out.println("Enter third number");
+        int number3 = sc.nextInt();
+        System.out.println("Addition of the numbers: "  +  (number + number2 + number3));
+        System.out.println("Subtraction of the numbers: " + (number - number2 - number3));
+        System.out.println("Multiplication of the numbers: " + (number * number2 * number3));
+        System.out.println("Division of the numbers: " + (number / number2 / number3));
+    }
+}
