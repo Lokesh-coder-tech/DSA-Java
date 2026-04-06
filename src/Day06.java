4a2a766d295a89ed515a36b6c80de5c69cec204b
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-06 15:10:02 +0530

    Understood loops in Java by solving questions

---
+++ src/Day06.java
@@ -0,0 +1,96 @@
+//Loops
+import java.util.Scanner;
+public class Day06 {
+    public static void main(String[] args) {
+        //for loop
+//        for(int i=0; i<3; i++){
+//           for(int j=0; j<3; j++){
+//               System.out.print("*");
+//           }
+//            System.out.println();
+//        }
+
+        //while loop
+//        int i=0;
+//        while(i<10){
+//            System.out.println("Jai Hind Dosto!!");
+//            i++;
+//        }
+
+        //do-while loop
+//        int i=0;
+//        do{
+//            System.out.println("Har Har Mahadev!!");
+//            i++;
+//        }while(i < 5);
+
+        //print your name 100 times--->
+//        for(int i=1; i<=100; i++ ){
+//            System.out.println("Lokesh");
+//        }
+
+        //print all even numbers from 1 to 100---->
+//        for(int i=1; i<=100; i++){
+//            if(i % 2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+
+        //print all prime numbers from 1 to 100---->
+//      for(int num = 2; num <= 100; num++){
+//         boolean isPrime = true;
+//         for(int i = 2; i <= Math.sqrt(num); i++){
+//             if(num % i == 0){
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//         if(isPrime){
+//             System.out.println(num + " ");
+//         }
+//      }
+
+        //print sum of all numbers from 1 to n---->
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int sum = 0;
+//        for(int i = 1; i<=n; i++){
+//             sum = sum + i;
+//        }
+//        System.out.println("sum is: " + sum);
+
+        //Print all integers in range from 50 to 100, that are perfectly divisible by 7--->
+//        for(int i = 50; i<=100; i++){
+//            if(i % 7 == 0){
+//                System.out.println(i);
+//            }
+//
+//        }
+
+        //Print counting from 1 to n--->
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i = 1; i <= n; i++){
+//            System.out.println(i);
+//        }
+
+        //Print counting from n to 1--->
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i = n; i>=1; i--){
+//            System.out.println(i);
+//        }
+
+        //Print the 10 multiples of n---->
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 1; i <= 10; i++){
+//            System.out.println(i * n);
+//        }
+
+//        sc.close();
+
+    }
+}
