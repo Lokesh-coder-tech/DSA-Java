f8276325c5849699ef0a603c1df04c6955b02620
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-12 22:50:08 +0530

    Understood methods in Java by solving questions

---
+++ src/Day08.java
@@ -0,0 +1,62 @@
+//methods in Java
+
+public class Day08 {
+
+    //CREATE A METHOD printWelcomeMessage() THAT PRINTS A GREETING--->
+//    static void printWelcomeMessage(){
+//        System.out.println("welcome the to my world");
+//    }
+
+    //CREATE A METHOD add(int a, int b) THAT RETURNS THE SUM----->
+//    static void add(int a, int b){
+//        System.out.println("sum: " + (a + b));
+//    }
+
+    //CREATE A METHOD isEven(int number) THAT RETURNS TRUE IF NUMBER IS EVEN.----->
+//    static void isEven(int number){
+//        if(number % 2 == 0){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+//    }
+
+    //CREATE A METHOD getMaximum(int a, int b) THAT RETURNS THE LARGER NUMBER--->
+//    static int getMaximum(int a, int b){
+//        return (a > b) ? a : b;
+//    }
+
+//    CREATE A METHOD calculatePercentage(int obtained, int total).
+//    static int calculatePercentage(int obtained, int total){
+//        return (obtained * 100) / total ;
+//    }
+
+    //CREATE AN OVERLOADED METHOD display() WITH ONE INT PARAMETER OR ONE STRING PARAMETER---->
+//    static void display(int a){
+//        System.out.println(a);
+//    }
+//    static void display(String b ){
+//        System.out.println(b);
+//    }
+
+    //WRITE A METHOD updateValue(int x) AND VERIFY THAT ORIGINAL VARIABLE IN main() DOES NOT CHANGE--->
+//    static void updateValue(int x){
+//         x = x + 10;
+//        System.out.println(x);
+//    }
+
+    public static void main(String[] args) {
+//        printWelcomeMessage();
+//        add(2, 4);
+//        isEven(4);
+//        int result = getMaximum(3, 4);
+//        System.out.println("result: " + result);
+//        int result = calculatePercentage(480, 500);
+//        System.out.println(result);
+//        display(12);
+//        display("jai hind dosto");
+//        int num = 30;
+//        updateValue(num);
+//        System.out.println(num);
+    }
+}
