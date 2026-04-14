1dcd732efbc8f9ca62c0227980bbc96d0d882577
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-14 10:25:34 +0530

    Understood methods in Java by solving questions

---
+++ src/Day09.java
@@ -0,0 +1,102 @@
+// Array Basics
+import java.util.Scanner;
+public class Day09 {
+    public static void main(String[] args) {
+
+        //creation- Declaration, Allocation and Initialization,
+//        int arr[];
+//        arr = new int[3];
+//        System.out.println(arr.length);
+
+//         int arr[] = {10, 20, 30};
+//         System.out.println("value at index 0: " + arr[0]);
+//         System.out.println("value at index 1: " + arr[1]);
+//         System.out.println("value at index 2: " + arr[2]);
+//
+//         System.out.print("elements of array is: ");
+//         for(int i=0; i< arr.length; i++){
+//             System.out.println(arr[i]);
+//         }
+
+
+        //Take input in array
+//        int arr[] = new int[5];
+//        int n = arr.length;
+//        Scanner sc = new Scanner(System.in);
+//
+//        for(int i=0; i<n; i++){
+//            System.out.println("Provide input for index: " + i);
+//            arr[i] = sc.nextInt();
+//        }
+//        for(int i=0; i<n; i++){
+//            System.out.println(arr[i]);
+//        }
+        //or
+//        System.out.println("Elements of array is: ");
+//        for(int val:arr){
+//            System.out.println(val);
+//        }
+
+
+        //Print Max value from array
+//        int arr[] = {5, 20, -4, 30, 10};
+//        int n = arr.length;
+//        int maxValue = arr[0];
+//
+//        for(int i=0; i<n; i++){
+//            if(arr[i] > maxValue){
+//                maxValue = arr[i];
+//            }
+//        }
+//        System.out.println("Max-value is: " + maxValue);
+
+
+        //Print Min value from array
+//        int arr[] = {5, 20, -4, 30, 10};
+//        int n = arr.length;
+//        int minValue = arr[0];
+//
+//        for(int i=0; i<n; i++){
+//            if(arr[i] < minValue){
+//                minValue = arr[i];
+//            }
+//        }
+//        System.out.println("Max-value is: " + minValue);
+
+        //2D-Array
+//        int arr[][];
+//        arr = new int[3][3];
+
+//        int arr[][] = {
+//                {1 ,2, 3},
+//                {3, 4, 5},
+//                {5, 6, 7}
+//        };
+//
+//       int rowLength = arr.length;
+//       int colLength = arr[0].length;
+//
+//       for(int i=0; i<rowLength; i++){
+//           for(int j=0; j<colLength; j++){
+//               System.out.print(arr[i][j] + " ");
+//           }
+//           System.out.println();
+//       }
+
+//        int arr[][] = {
+//                {1, 4, 5},
+//                {5, 6, 7, 8},
+//                {9, 10, 11, 12, 13},
+//                {14}
+//        };
+//        int rowLength = arr.length;
+//
+//        for(int i=0; i<rowLength; i++){
+//          int colLength = arr[i].length;
+//           for(int j=0; j<colLength; j++){
+//               System.out.print(arr[i][j] + " ");
+//          }
+//            System.out.println();
+//        }
+    }
+}
