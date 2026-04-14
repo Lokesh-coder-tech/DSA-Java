caf2f869f7896c7665395878eb956e0c02121da4
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-14 10:26:23 +0530

    Understood Array basics in Java

--- src/Day09.java
+++ src/Day09.java
@@ -17,8 +17,7 @@ public class Day09 {
 //         for(int i=0; i< arr.length; i++){
 //             System.out.println(arr[i]);
 //         }
-
-
+        
         //Take input in array
 //        int arr[] = new int[5];
 //        int n = arr.length;
