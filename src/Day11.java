d7b3e1cff4bdb1369fbaf88892440a4acde7d641
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-16 11:08:40 +0530

    Understood Basic maths in DSA

---
+++ src/Day11.java
@@ -0,0 +1,78 @@
+//Basic Maths for DSA
+
+public class Day11 {
+    //Print Digits of a number
+//    static void printDigits(int num){
+//        while(num != 0){
+//            int digit = num % 10;
+//            System.out.println(digit);
+//            //last digit remove
+//            num = num/10;
+//        }
+//    }
+
+    //Count Digits of a Number
+//    static int countDigits(int num){
+//        int count = 0;
+//        while(num != 0){
+//            int digit = num % 10;
+//            count++;
+//            //last digit remove
+//            num = num/10;
+//        }
+//        return count;
+//    }
+
+    //Sum of Digits of a Number
+//    static int sumOfDigits(int num){
+//        int sum = 0;
+//        while(num != 0) {
+//            int digit = num % 10;
+//            sum = sum + digit;
+//            num = num/10;
+//        }
+//        return sum;
+//    }
+
+    //Reverse a number
+//    static int reverseNumber(int num){
+//        int reverse = 0;
+//        while(num!= 0 ){
+//            int digit = num % 10;
+//            reverse = reverse * 10 + digit;
+//            num = num /10;
+//        }
+//        return reverse;
+//    }
+
+    //check Palindrome number
+//        static int palindromeNumber(int num){
+//        int reverse = 0;
+//        int originalNum = num;
+//        while(num!= 0 ){
+//            int digit = num % 10;
+//            reverse = reverse * 10 + digit;
+//            num = num /10;
+//        }
+//            if(originalNum == reverse){
+//                System.out.println("yes it is palindrome");
+//            }else{
+//                System.out.println("No it is not a palindrome");
+//            }
+//        return reverse;
+//    }
+
+    public static void main(String[] args) {
+//        int num = 53127;
+//        printDigits(num);
+//        int ans = countDigits(num);
+//        System.out.println(ans);
+//        int sum = sumOfDigits(num);
+//        System.out.println(sum);
+//        int num = 1498;
+//        int ans = reverseNumber(num);
+//        System.out.println(ans);
+//          int num = 1221;
+//          palindromeNumber(num);
+    }
+}
