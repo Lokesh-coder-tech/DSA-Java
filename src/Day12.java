8f73bfb22e96062452ada614dcd3647fab0fa468
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-04-18 09:52:15 +0530

    Understood OOP-class and object in Java

---
+++ src/Day12.java
@@ -0,0 +1,69 @@
+//OOP(Object Oriented Programming) in Java - 01
+
+//Classes and objects
+
+//class Student{
+//    String name;
+//    int age;
+//
+//    void study(){
+//        System.out.println(name + " is a stuednt");
+//        System.out.println(name + " age is " + age);
+//    }
+//}
+//
+
+//Create a Car class with brand, speed Add method drive(), Create 2 objects
+//class Car {
+//    String brand;
+//    String speed;
+//
+//    void drive() {
+//        System.out.println("The car brand is " + brand + " speeed is " + speed);
+//    }
+//}
+
+//Create a Mobile class properties: brand, price, model, camera, battery and method: call(), create 2 objects with different values
+//class Mobile{
+//    String brand;
+//    int price;
+//    String model;
+//    String camera;
+//    String battery;
+//
+//    void call(){
+//        System.out.println("The Phone is: " + brand);
+//        System.out.println("Price is: " + price);
+//        System.out.println("Model: " + model);
+//        System.out.println("Camera Quality: " + camera);
+//        System.out.println("Battery Power: " + battery);
+//    }
+//}
+
+public class Day12 {
+    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.name = "Lokesh";
+//        s1.age = 20;
+//
+//        s1.study();
+//        Car c1 = new Car();
+//        c1.brand = "G-Wagon";
+//        c1.speed = "200km/h";
+//
+//        c1.drive();
+
+//        Mobile m1 = new Mobile();
+//        m1.brand = "Realme";
+//        m1.price = 20000;
+//        m1.model = "P1-5g";
+//        m1.camera = "50 mega-px";
+//        m1.battery = "5000 mah";
+//
+//        m1.call();
+
+    }
+}
+
+
+
