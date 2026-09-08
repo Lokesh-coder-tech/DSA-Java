b1ea3b9b65223e0f2dc3ba5f9f7e21f0d3adc551
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-08 09:55:25 +0530

    Revising java concepts-Day2

---
+++ src/Revision2.java
@@ -0,0 +1,103 @@
+public class Revision2 {
+    public static void main(String[] args) {
+//        int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
+//
+//        for(int i=0; i<arr.length; i++){
+////            System.out.println("I Love Ujjain");
+////            System.out.println(i);
+//            System.out.println(arr[i]);
+//        }
+
+//        int i = 1;
+//        while(i < 6){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while(i < 6);
+
+//        for(int i = 1; i<=10; i++) {
+//            if(i == 5 ) {
+//                continue;
+//            }
+//            if(i == 7){
+//                continue;
+//            }
+//            System.out.println(i);
+//
+//        }
+
+//        for(int i = 1; i <= 10; i++){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 10; i>=1; i--){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 1; i<=10; i++){
+//            if(i % 2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+
+//        int sum = 0;
+//        for(int i = 1; i<=10; i++){
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+//        int num = 0;
+//        if(num < 0) {
+//            System.out.println("Number is negative");
+//        } else if (num == 0) {
+//            System.out.println("Number is zero");
+//        } else{
+//            System.out.println("number is positive");
+//        }
+//
+//        int arr[] = {-4 , 0, 4};
+//
+//        for(int i = 0; i < arr.length; i++) {
+//            if(arr[i] < 0){
+//                System.out.println("Negative Number");
+//            } else if (arr[i] == 0) {
+//                System.out.println("Number is zero");
+//            }else{
+//                System.out.println("Positive number");
+//            }
+//        }
+
+//        int sum = 0;
+//        for(int i = 1; i <= 10; i++){
+//            if(i % 2 == 0){
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+
+//        int count = 0;
+//        for(int i = 1; i<=10; i++){
+//            if(i % 3 == 0){
+//               count++;
+//            }
+//        }
+//        System.out.println(count);
+
+
+//        int arr[] = {12, 14, 19, 8, 10};
+//        int max = arr[0];
+//        for(int i = 0; i<arr.length; i++){
+//
+//             if(arr[i] > max){
+//                 max = arr[i];
+//             }
+//
+//        }
+//        System.out.println(max);
+    }
+}
