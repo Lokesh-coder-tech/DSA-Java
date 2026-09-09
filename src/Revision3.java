534cddac09102a7091f0d1492e099d2c8abfd398
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-09 10:03:53 +0530

    Revising java concepts-Day3

---
+++ src/Revision3.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Revision3 {
+
+//    static void greet(){
+//        System.out.println("Hello Dosto!!");
+//    }
+//    static void intro(){
+//        System.out.println("My name is Lokesh Sharma");
+//    }
+
+//    static int add(int a, int b) {
+//       System.out.println(a + b);
+//       return a + b;
+//    }
+
+//    static int multiply(int c, int d){
+//        return c * d;
+//    }
+
+//    static int maxNumber(int a, int b, int c){
+//        int max = a;
+//        if(b > max){
+//            max = b;
+//        };
+//        if(c > max){
+//            max = c;
+//        };
+//        return max;
+//    }
+
+//    static int maxNumber(int arr[]){
+//        int max = arr[0];
+//        for(int i = 0; i<arr.length; i++){
+//            if(arr[i] > max){
+//                max = arr[i];
+//            }
+//        }
+//        return max;
+//    }
+
+//    static int isEven(int n){
+//        if(n % 2 == 0){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+//        return n;
+//    }
+
+//    static int sumOfArray(int arr[]){
+//        int sum = 0;
+//        for(int i = 0; i<arr.length; i++){
+//            sum += arr[i];
+//        }
+//        return sum;
+//    }
+
+    public static void main(String[] args) {
+//       greet();
+//       intro();
+//       add(2, 3);
+//       int result = multiply(5, 4);
+//       System.out.println(result);
+//       int maxNumber = maxNumber(12, 14, 6);
+//       System.out.println(maxNumber);
+//       int[] arr = {1, 4, 6, 9, 5};
+//       int ans = maxNumber(arr);
+//       System.out.println(ans);
+//       Scanner sc = new Scanner(System.in);
+//       int num = sc.nextInt();
+//       int result = isEven(num);
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Array Size: ");
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         System.out.println("Array Elements: ");
+//         for(int i = 0; i<arr.length; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int sumOfArray = sumOfArray(arr);
+//         System.out.println("Sum of Array is: " + sumOfArray);
+
+
+
+    }
+}
