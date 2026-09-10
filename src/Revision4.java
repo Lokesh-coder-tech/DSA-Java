7625db0b875beefa057b904a091b2630b10cfe32
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-10 09:21:20 +0530

    Revising java concepts-Day4

---
+++ src/Revision4.java
@@ -0,0 +1,74 @@
+//Method Overloading
+
+public class Revision4 {
+
+//    static int addition(int a, int b){
+//        return a + b;
+//    };
+//    static int additon(int a, int b, int c){
+//        return a + b + c;
+//    };
+
+//    static double addition(double a, double b){
+//        return a + b;
+//    };
+//
+//    static int addition(int a, int b, int c){
+//        return a + b + c;
+//    };
+
+//    static int subtraction(int a, int b){
+//        return a - b;
+//    };
+//    static double subtraction(double a, double b){
+//        return b - a;
+//    };
+
+//    static int area(int side){
+//        return side * side ;
+//    };
+//    static int area(int length, int breadth ){
+//        return length * breadth;
+//    };
+
+//    static int max(int a, int b){
+//        int max = a;
+//        if(b > max){
+//            max = b;
+//        };
+//        return max;
+//    };
+//    static int max(int a, int b, int c){
+//        int max = a;
+//        if(b > max){
+//            max = b;
+//        };
+//        if(c > max){
+//            max = c;
+//        };
+//        return max;
+//    };
+//    static int max(int[] arr){
+//        int max = arr[0];
+//        for(int i = 0; i< arr.length; i++){
+//            if(arr[i] > max){
+//                max = arr[i];
+//            };
+//        };
+//        return max;
+//    };
+
+    public static void main(String[] args) {
+//       System.out.println(addition(2.1,3.2));
+//       System.out.println(addition(2,3,5));
+//       System.out.println(subtraction(5, 1));
+//       System.out.println(subtraction(2.2,6.2));
+//       System.out.println(area(4));
+//       System.out.println(area(2, 4));
+//       System.out.println(max(6, 5));
+//       System.out.println(max(2, 3, 1));
+//       int[] arr = {12, 13, 10};
+//       System.out.println(max(arr));
+
+    }
+}
