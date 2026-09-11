3a833a7d9e34dce28a0e815edb78b83e8fd6c9d9
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-11 10:17:47 +0530

    Revising java concepts-Day5 - Complete Array Revision Include 2d-Array, Arraylist with some questions.

---
+++ src/Revision5.java
@@ -0,0 +1,127 @@
+//Array and String
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Revision5 {
+
+    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 4};
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Size of the Array: ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Elements of the Array: ");
+//        for(int i = 0; i< arr.length; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.print("Your Array: " + "[");
+//        for(int i = 0; i< arr.length; i++){
+//            System.out.print(arr[i]);
+//            if(i < arr.length - 1){
+//                System.out.print(",");
+//            }
+//        }
+//        System.out.print("]");
+
+//        int[] arr = {1, 2, 4, 5, 6};
+//        for(int num : arr){
+//            System.out.println(num);
+//        }
+
+//        int[] arr = {10, 30, 40};
+//        arr[1] = 50;
+//        for(int num : arr){
+//            System.out.println(num);
+//        }
+
+//        int[][] arr = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        for(int i = 0; i< arr.length; i++){
+//           for(int j = 0; j<arr[i].length; j++){
+//               System.out.print(arr[i][j] + " ");
+//           }
+//            System.out.println();
+//        };
+
+//        ArrayList <Integer> list = new ArrayList<>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(2,30);
+//        list.add(70);
+//
+//        list.set(1, 50);
+//        list.set(2, 60);
+//        System.out.print("[");
+//        System.out.print( list.get(0) + ", ");
+//        System.out.print( list.get(1) + ", ");
+//        System.out.print( list.get(2) + ", ");
+//        System.out.print(list.get(3));
+//        System.out.println("]");
+//        System.out.println("Arraylist size: " + list.size());
+
+//        ArrayList <Integer> list = new ArrayList<>();
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        list.add(6);
+//
+//
+//        System.out.print("[");
+//        for(int i = 0; i< list.size(); i++){
+//            System.out.print(list.get(i));
+//            if(list.get(i) < list.size() + 1){
+//                System.out.print(",");
+//            }
+//        }
+//        System.out.println(']');
+//
+//        if(list.contains(5)){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+//        if(list.isEmpty()){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+
+
+//        for(int num : list){
+//            System.out.println(num);
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Size of Arraylist: ");
+//        int n = sc.nextInt();
+//        System.out.println("Elements of list: ");
+//        ArrayList <Integer> list = new ArrayList<>();
+//        for(int i = 0; i<n; i++){
+//            list.add(sc.nextInt());
+//        }
+//        System.out.println("Your Arraylist: " + list);
+//        int max = list.get(0);
+//        int min = list.get(0);
+//        for(int i = 0; i<n; i++){
+//            if(list.get(i) > max){
+//                max = list.get(i);
+//            }
+//            if(list.get(i) < min){
+//                min = list.get(i);
+//            }
+//        }
+//        System.out.println("Maximum Element in the Arraylist: " + max);
+//        System.out.println("Minimun Element in the Arraylist: " + min);
+//
+
+    }
+
+}
