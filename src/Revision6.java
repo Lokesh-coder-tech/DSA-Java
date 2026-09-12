8a2f68918d7147eb33af6dc9a84f770fc6217f8b
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-12 10:04:51 +0530

    Revising java concepts-Day6-Strings and its methods with some questions.

---
+++ src/Revision6.java
@@ -0,0 +1,77 @@
+//Strings
+
+public class Revision6 {
+    public static void main(String[] args) {
+//        String name = "Lokesh";
+//        System.out.println(name);
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.charAt(2));
+//        System.out.println(name.length());
+
+        //Strings Methods
+             //charAt()
+//        String s = "Hello";
+//        System.out.println(s.charAt(2));
+            //substring()
+//        System.out.println(s.substring(1, 4));
+            //equals()
+//        String a = "Namaste";
+//        String b = "Namaste";
+//        System.out.println(b.equals(a));
+            //toUpperCase(), toLowerCase()
+//        String l = "lucky";
+//        String m = "DHONI";
+//        System.out.println(l.toUpperCase());
+//        System.out.println(m.toLowerCase());
+//            //contains()
+//        System.out.println(l.contains("ue"));
+              //indexOf()
+//        System.out.println(l.indexOf('y'));
+
+              //String is Immutable
+//        String s = "Hello";
+//        s = s.concat(" World");
+//        System.out.println(s);
+
+             //String-Builder
+//        StringBuilder sb = new StringBuilder("Hollo");
+//        sb.append(" world");
+//        sb.reverse();
+//        sb.setCharAt(1,'e');
+//        System.out.println(sb);
+
+//        String sg = "Lakshya";
+
+//        System.out.println(sg.charAt(0));
+//        System.out.println(sg.charAt(1));
+//        System.out.println(sg.charAt(2));
+//        System.out.println(sg.charAt(3));
+//        System.out.println(sg.charAt(4));
+//        System.out.println(sg.charAt(5));
+//        System.out.println(sg.charAt(6));
+
+//        String st = "Lokesh";
+//        int count = 0;
+//            if(st.contains("a")){
+//                count++;
+//            };
+//            if(st.contains( "e" )){
+//                count++;
+//            };
+//            if(st.contains( "i" )){
+//                count++;
+//            };
+//            if(st.contains( "o" )){
+//                count++;
+//            };
+//            if(st.contains( "u" )){
+//                count++;
+//            };
+//        System.out.println(count);
+
+//        StringBuilder sg = new StringBuilder("Hello");
+//        sg.reverse();
+//        System.out.println(sg);
+
+    }
+}
