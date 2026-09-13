bc9ea8d390aa7c4306ea1c95e4932589874ccb3c
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-13 10:54:07 +0530

    Revising java concepts-Day7-Hashmap.

---
+++ src/Revision7.java
@@ -0,0 +1,35 @@
+//Hashmap & Hashset
+
+import java.util.HashMap;
+import java.util.Map;
+public class Revision7 {
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1,"Lokesh");
+        map.put(2, "Lucky");
+        map.put(3, "Lakshya");
+//        map.remove(2);
+
+//        System.out.println(map.get(1));
+//        System.out.println(map.get(2));
+//        System.out.println(map.get(3));
+//        System.out.println(map.containsKey(3));
+//        System.out.println(map.containsValue("Lucky"));
+//        System.out.println(map.size());
+
+//        for(Integer key : map.keySet()){
+//            System.out.println(key);
+//        };
+//        for(String value : map.values()){
+//            System.out.println(value);
+//        }
+
+//        for(Map.Entry<Integer, String> entry : map.entrySet() ){
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+
+
+    }
+
+}
