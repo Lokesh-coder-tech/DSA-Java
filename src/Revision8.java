26e149e7827a4965b37022c143ea4226c08c7fe6
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-15 10:27:29 +0530

    Revising java concepts-Day7-Class,object and OOP concepts.

---
+++ src/Revision8.java
@@ -0,0 +1,146 @@
+//Class, Object and OOPs concept
+
+//class Student{
+//    String name;
+//    int age;
+//    String address;
+//    int num;
+//}
+
+//class Car{
+//    String name;
+//    String model;
+//    int price;
+//}
+
+//Encapsulation-->
+//class BankAccount{
+//    private int balance;
+//
+//    public void setBalance(int amount){
+//        balance = balance + amount;
+//    }
+//    public int getBalance(){
+//        return balance;
+//    }
+//}
+
+//Inheritance-->
+//class Animal{
+//    void eat(){
+//        System.out.println("Animals can eat");
+//    }
+//}
+//
+//class Dog extends Animal{
+//    void bark(){
+//        System.out.println("Dog is Barking");
+//    }
+//}
+
+//Polymorphism
+//Method-Overloading
+//class Calculator{
+//     int add(int a, int b){
+//         return a + b;
+//     };
+//     int add(int a, int b, int c){
+//         return a + b + c;
+//     }
+//}
+
+//Method-Overriding
+//class Animal{
+//    void sound(){
+//        System.out.println("Animal can sound");
+//    }
+//}
+//class Dog extends Animal{
+//
+//    @Override
+//    void sound(){
+//        System.out.println("Dogs barks");
+//    }
+//}
+
+//Abstraction
+//abstract class Animal{
+//    abstract void sound();
+//
+//    void eat(){
+//        System.out.println("Animals can eat");
+//    };
+//};
+//
+// class Dog extends Animal{
+//    void sound() {
+//        System.out.println("Dogs barks");
+//    }
+//};
+
+public class Revision8 {
+    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        Student s2 = new Student();
+//        s1.name = "Lokesh";
+//        s1.age = 20;
+//        s1.address = "25, Swastik Nagar.";
+//        s1.num = 234567890;
+//
+//        s2.name = "Lucky";
+//        s2.age = 21;
+//        s2.address = "26, Swastik Nagar.";
+//        s2.num =123456789;
+//
+//        System.out.println("Bio-data---------------->");
+//        System.out.println(" ");
+//        System.out.println("Name: " + s1.name);
+//        System.out.println("Age: " + s1.age);
+//        System.out.println("Address: " + s1.address);
+//        System.out.println("Number: " + s1.num);
+//        System.out.println(" ");
+//        System.out.println("Name: " + s2.name);
+//        System.out.println("Age: " + s2.age);
+//        System.out.println("Address: " + s2.address);
+//        System.out.println("Number: " + s2.num);
+
+//        Car c1 = new Car();
+//        Car c2 = new Car();
+//
+//        c1.name = "Maruti-Alto";
+//        c1.model="350";
+//        c1.price = 500000;
+//        c2.name = "Kia";
+//        c2.model="seltos";
+//        c2.price = 1000000;
+//
+//        System.out.println("Cars---------------->");
+//        System.out.println(" ");
+//        System.out.println("Name: " + c1.name);
+//        System.out.println("Model: " + c1.model);
+//        System.out.println("Price: " + c1.price);
+//        System.out.println(" ");
+//        System.out.println("Name: " + c2.name);
+//        System.out.println("Model: " + c2.model);
+//        System.out.println("Price: " + c2.price);
+
+//        BankAccount account = new BankAccount();
+//        account.setBalance(20000);
+//        System.out.println(account.getBalance());
+
+//         Dog d =  new Dog();
+//         d.eat();
+//         d.bark();
+
+//         Calculator c = new Calculator();
+//         System.out.println(c.add(2,3));
+//         System.out.println(c.add(2, 3, 5));
+
+//        Animal a = new Dog();
+//        a.sound();
+
+//          Dog d = new Dog();
+//          d.sound();
+
+    }
+}
