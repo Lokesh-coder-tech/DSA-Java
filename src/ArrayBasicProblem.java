14a1f0d21fe279040823d39a4540cdebaf2a04da
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-09-19 09:33:56 +0530

    Solved some Array basic problems

---
+++ src/ArrayBasicProblem.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class ArrayBasicProblem {
+
+    public static void main(String[] args) {
+
+ //Find the maximun element and minimum elements--------->
+
+//        int[] arr = {10, 20, 40, 30, 70, 15, 12, 19};
+//        int max = arr[0];
+//        for(int i=0; i< arr.length; i++){
+//            if(arr[1] > max){
+//                max = arr[1];
+//            };
+//            if(arr[2] > max){
+//                max = arr[2];
+//            };
+//            if(arr[3] > max){
+//                max = arr[3];
+//            };
+//        };
+//        System.out.println(max);
+
+//        int[] arr = {10, 20, 4, 50};
+//        int min = arr[0];
+//        for(int i=0; i< arr.length; i++){
+//            if(arr[i] < min){
+//                min = arr[i];
+//            };
+//        };
+//        System.out.println(min);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Size of the Array: ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Elements of Array: ");
+//        for(int i=0; i< arr.length; i++){
+//            arr[i] = sc.nextInt();
+//        };
+//        int min = arr[0];
+//        for(int i = 0; i< arr.length; i++){
+//            if(arr[i] < min){
+//                min = arr[i];
+//            };
+//        }
+
+//        int min  = Integer.MAX_VALUE;
+//        for(int i = 0; i<n; i++){
+//            int num = sc.nextInt();
+//            if(num < min){
+//                min = num;
+//            };
+//        };
+//        System.out.println(min);
+//
+
+//        System.out.println("Elements of the Array: ");
+//        int max = Integer.MIN_VALUE;
+//        for(int i = 0; i<n; i++){
+//            int num  = sc.nextInt();
+//            if(num > max){
+//                max = num;
+//            };
+//        };
+//        System.out.println("maximum element out of the array: " + max);
+
+//
+//        System.out.println("Minimum element out of the Array: " + min);
+
+//Find the sum------------>
+
+//        int[] arr = {1, 2, 3, 4, 5};
+//        int sum = 0;
+//        for(int i = 0; i< arr.length; i++){
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum );
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        int sum  = 0;
+//        for(int i = 0; i<n; i++){
+//            arr[i] = sc.nextInt();
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//
+
+//Count How many Even numbers are present--------->
+
+//        int[] arr = {1, 2, 4, 5, 8};
+//        int count = 0;
+//        for(int i = 0; i< arr.length; i++){
+//            if(arr[i] % 2 == 0){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+//Linear Search - search for an element------->
+
+//        int arr[] = {10, 15, 25, 30, 40};
+//        for(int i = 0; i< arr.length; i++){
+//            if(arr[i] == 30){
+//                System.out.println("Index: " + i);
+//            };
+//        };
+    }
+}
